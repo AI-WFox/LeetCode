1b3a553155bc7e6a018b37d31d7c59de77e808d6
White Fox <84433497+AI-WFox@users.noreply.github.com>
2023-09-21 07:49:55 +0000

    [LeetCode Sync] Runtime - 11 ms (2.02%), Memory - 23.6 MB (100.00%)

---
+++ my-solutions/0344-reverse-string/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void reverse(vector<char>& s, int left, int right) {
+        if (left<right) {
+            swap(s[left],s[right]);
+            reverse (s, left + 1, right - 1);
+        }
+    }
+    void reverseString(vector<char>& s) {
+        reverse (s,0,s.size()-1);
+    }
+};
